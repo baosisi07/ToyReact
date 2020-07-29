c8b10e55e57e8ff6c4410b6780fe1b486bda7944
baoss <18018021075@163.com>
2020-07-29 15:22:49 +0800

    component的DOM渲染实现

--- src/MyReact.js
+++ src/MyReact.js
@@ -1,16 +1,71 @@
+class wrapperElement {
+    constructor(type) {
+        this.root = document.createElement(type)
+    }
+    setAttribute(name,value) {
+        this.root.setAttribute(name,value)
+    }
+    appendChild(child) {
+        child.mountedTo(this.root)
+    }
+    mountedTo(parent) {
+        parent.appendChild(this.root)
+    }
+}
+class wrapperTextElement {
+    constructor(content) {
+        this.root = document.createTextNode(content)
+    }
+    mountedTo(parent) {
+        parent.appendChild(this.root)
+    }
+}
+export class Component {
+    constructor() {
+        this.children = []
+    }
+    setAttribute(name, value) {
+        this[name] = value
+    }
+    appendChild(vChild) {
+        this.children.push(vChild)
+    }
+    mountedTo(parent) {
+        let vDom = this.render()
+        vDom.mountedTo(parent)
+    }
+}
 export let MyReact = {
     createElement(type,attributes,...children) {
-        console.log(arguments)
-        let ele = document.createElement(type)
+        let ele
+        if(typeof type === 'string') {
+            ele = new wrapperElement(type)
+        } else {
+            ele = new type
+        }
         for(let attr in attributes) {
             ele.setAttribute(attr,attributes[attr])
         }
-        for(let child of children) {
-            if(typeof child === 'string') {
-                child = document.createTextNode(child)
+        let insertChild = (children) => {
+            for (let child of children) {
+                if(typeof child === 'object' && child instanceof Array) {
+                    insertChild(child)
+                } else {
+                    if (!(child instanceof Component) && !(child instanceof wrapperElement) && !(child instanceof wrapperTextElement)) {
+                        child = String(child)
+                    }
+
+                    if (typeof child === 'string') {
+                        child = new wrapperTextElement(child)
+                    }
+                    ele.appendChild(child)
+                } 
             }
-            ele.appendChild(child)
         }
+        insertChild(children)
         return ele
+    },
+    render(vDom,parent) {
+        vDom.mountedTo(parent)
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import {MyReact} from './MyReact'
+import {MyReact, Component} from './MyReact'
 
-// class MyComponent {
+class MyComponent extends Component {
+    render() {
+    return <div class="attr"><span>hello </span><span>world!</span><p>{false}</p>{this.children}</div>
+    }
+    
+}
+let a = <MyComponent name="ss"><div>你好</div></MyComponent>
 
-// }
-// let a = <MyComponent name="ss" />
+MyReact.render(a, document.getElementById('root'))
+// let b = <div class="ss">
+//     <span class="f">233</span>
+//     <span>hello</span>
+// </div>
 
-let b = <div class="ss">
-    <span class="f">233</span>
-    <span>hello</span>
-</div>
-
-let root = document.getElementById('root');
-root.appendChild(b)
+// let root = document.getElementById('root');
+// root.appendChild(b)
